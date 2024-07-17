0e0979535ceefdc5dcf1310d06482bb8a92d0d96
12345zsk <129924442+12345zsk@users.noreply.github.com>
2024-07-17 15:11:05 +0800

    Lab 4

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -12,7 +12,7 @@ sys_exit(void)
   int n;
   argint(0, &n);
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -43,7 +43,7 @@ sys_sbrk(void)
 
   argint(0, &n);
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
@@ -55,18 +55,21 @@ sys_sleep(void)
   uint ticks0;
 
   argint(0, &n);
-  if(n < 0)
+  if (n < 0)
     n = 0;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(killed(myproc())){
+  while (ticks - ticks0 < n)
+  {
+    if (killed(myproc()))
+    {
       release(&tickslock);
       return -1;
     }
     sleep(&ticks, &tickslock);
   }
   release(&tickslock);
+  backtrace();
   return 0;
 }
 
@@ -90,4 +93,67 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64
+sys_sigalarm(void)
+{
+  int ticks;
+  uint64 handler;
+
+  argint(0, &ticks);
+  argaddr(1, &handler);
+
+  struct proc *p = myproc();
+  p->ticks = ticks;
+  p->ticks_cnt = 0;
+  p->handler = handler;
+
+  return 0;
+}
+
+void restore()
+{
+  struct proc *p = myproc();
+  p->trapframe->ra = p->tick_ra;
+  p->trapframe->sp = p->tick_sp;
+  p->trapframe->gp = p->tick_gp;
+  p->trapframe->tp = p->tick_tp;
+  p->trapframe->t0 = p->tick_t0;
+  p->trapframe->t1 = p->tick_t1;
+  p->trapframe->t2 = p->tick_t2;
+  p->trapframe->s0 = p->tick_s0;
+  p->trapframe->s1 = p->tick_s1;
+  p->trapframe->a0 = p->tick_a0;
+  p->trapframe->a1 = p->tick_a1;
+  p->trapframe->a2 = p->tick_a2;
+  p->trapframe->a3 = p->tick_a3;
+  p->trapframe->a4 = p->tick_a4;
+  p->trapframe->a5 = p->tick_a5;
+  p->trapframe->a6 = p->tick_a6;
+  p->trapframe->a7 = p->tick_a7;
+  p->trapframe->s2 = p->tick_s2;
+  p->trapframe->s3 = p->tick_s3;
+  p->trapframe->s4 = p->tick_s4;
+  p->trapframe->s5 = p->tick_s5;
+  p->trapframe->s6 = p->tick_s6;
+  p->trapframe->s7 = p->tick_s7;
+  p->trapframe->s8 = p->tick_s8;
+  p->trapframe->s9 = p->tick_s9;
+  p->trapframe->s10 = p->tick_s10;
+  p->trapframe->s11 = p->tick_s11;
+  p->trapframe->t3 = p->tick_t3;
+  p->trapframe->t4 = p->tick_t4;
+  p->trapframe->t5 = p->tick_t5;
+  p->trapframe->t6 = p->tick_t6;
+}
+
+uint64
+sys_sigreturn(void)
+{
+  struct proc *p = myproc();
+  p->trapframe->epc = p->tick_epc;
+  restore();
+  p->handler_executing = 0;
+  return p->trapframe->a0;
 }
